6228a21aa3bfb3b2d1f5103d45f8719ecee534eb
Mirza Abdur Rehman <126644528+MirzaAbdurRehman@users.noreply.github.com>
2024-01-26 23:15:58 +0500

    React APP

---
+++ module1.mjs
@@ -0,0 +1,12 @@
+
+const  a = "Ali";
+const  b = "Mirza Abdur Rehman";
+const  c = "Ali Hasham";
+const  d = "Hadi";
+
+
+ //  JAVA SCRIPT CONCEPT
+ 
+export default d;  // Default Export 
+export { a, b, c }; // NameSpace Export 
+
